af814c908d9a1b0cb51f0b1909eb6e885d5a7c81
Yamojr001 <yamojr001@gmail.com>
2025-11-12 16:26:37 +0100

    final

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import inspect from "vite-plugin-inspect";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), inspect()],
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
